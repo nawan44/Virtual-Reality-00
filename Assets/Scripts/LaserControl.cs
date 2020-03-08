9d140e45d2e91e1d6e362f320d6b1a40f4c3528a
nawan44 <rachmat.d.gunawan@gmail.com>
2020-03-08 11:38:09 +0700

    Final Sub

---
+++ Assets/Scripts/LaserControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserControl : MonoBehaviour
+{
+
+    private LineRenderer laser;
+
+    // Use this for initialization
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // posisi laser yang dekat dengan camera
+        laser.SetPosition(0, transform.position);
+
+        // mengecek object yang terdapat didepan camera
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            // mengecek object yang dideteksi harus memiliki collider
+            if (hit.collider)
+            {
+                //jarak ujung laser berdasarkan posisi laser terkena object
+                laser.SetPosition(1, hit.point);
+              
+                // pastikan object yang dideteksi memiliki tag UI
+                if (hit.transform.tag.Equals("UI"))
+                {
+                    hit.transform.GetComponent<RespondButton>().SetHit();
+                    
+                    // ketika user menekan/klik tombol
+                    if (OVRInput.GetDown(OVRInput.RawButton.A))
+                    {
+                        hit.transform.GetComponent<RespondButton>().SetClick();
+                    }
+                }
+            }
+        }
+        else
+        {
+            // memberi batasan jarak laser terjauh dengan nilai maksimal 5000
+            laser.SetPosition(1, transform.forward * 5000);
+        }
+    }
+}
---
+++ Submission Akhir/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour {
+
+    public void MenuScene() {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Menu");
+    }
+
+    public void StartScene() {
+        SceneManager.LoadScene("Start");
+        Debug.Log("Mulai Permainan");
+    }
+    public void AlertQuitScene() {
+        SceneManager.LoadScene("Alert Quit");
+        Debug.Log("Alert");
+    }
+
+    public void keluar() {
+        Application.Quit();
+         Debug.Log("Quit");
+    }
+    
+}
